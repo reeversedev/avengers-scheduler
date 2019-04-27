83609e661d038bccdc221a840faa90b6776ddfb2
Idrees Dargahwala <idrees@theiyd.com>
2019-04-27 22:19:44 +0530

    Add Mumbai and Telegram messaging support
    For Hyderabad and Bangalore an email is sent, here in Mumbai a message on Telegram is sent.
    
    PS: IMAX Screen in Mumbai is going full instantly. Needed this. A great open source project xD

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var nodemailer = require('nodemailer');
-var fs = require('fs');
-var http = require('http');
+var request = require("request");
+var cheerio = require("cheerio");
+var nodemailer = require("nodemailer");
+var fs = require("fs");
+var telegram = require("telegram-bot-api");
+var util = require("util");
+var http = require("http");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Setting Timer for 5 minutes
 
@@ -14,13 +16,15 @@ let timer = 1000;
 
 let bangalore = 3;
 let hyderabad = 3;
+let mumbai = 3;
+let count = 0;
 
 // Handle Bangalore Requests
 
 function sendBangaloreMail() {
   if (process.env.BANGALORE_RECIPIENTS) {
     var transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: "gmail",
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
@@ -30,9 +34,9 @@ function sendBangaloreMail() {
     const mailOptions = {
       from: process.env.EMAIL,
       to: process.env.BANGALORE_RECIPIENTS,
-      subject: 'Avengers is here.',
+      subject: "Avengers is here.",
       html:
-        '<p>Hey! This is a reminder that new theatre has started screening Avengers: End Game</p>'
+        "<p>Hey! This is a reminder that new theatre has started screening Avengers: End Game</p>"
     };
 
     return transporter.sendMail(mailOptions, (err, info) => {
@@ -48,23 +52,29 @@ function sendBangaloreMail() {
 }
 
 async function doBangaloreStuff() {
-  console.log('Doing Bangalore...');
+  console.log("Doing Bangalore...");
   clearInterval(timer);
 
   setInterval(async () => {
-    console.log('Making the request');
-    await request(
-      'https://in.bookmyshow.com/buytickets/avengers-endgame-bengaluru/movie-bang-ET00100674-MT/20190426',
-      (error, response, html) => {
-        if (!error & (response.statusCode == 200)) {
-          var $ = cheerio.load(html);
-          if ($('#venuelist').children('.list').length > bangalore) {
-            bangalore++;
-            return sendBangaloreMail();
+    console.log("Making the request");
+    try {
+      await request(
+        "https://in.bookmyshow.com/buytickets/avengers-endgame-bengaluru/movie-bang-ET00100674-MT/20190426",
+        (error, response, html) => {
+          if (response) {
+            if (!error & (response.statusCode == 200)) {
+              var $ = cheerio.load(html);
+              if ($("#venuelist").children(".list").length > bangalore) {
+                bangalore++;
+                return sendBangaloreMail();
+              }
+            }
           }
         }
-      }
-    );
+      );
+    } catch (err) {
+      console.log(err);
+    }
   }, timer);
 
   return true;
@@ -75,7 +85,7 @@ async function doBangaloreStuff() {
 function sendHyderabadMail() {
   if (process.env.HYDERABAD_RECIPIENTS) {
     var transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: "gmail",
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
@@ -83,9 +93,9 @@ function sendHyderabadMail() {
     });
 
     const mailOptions = {
-      from: 'reeversedev@gmail.com',
+      from: "reeversedev@gmail.com",
       to: process.env.HYDERABAD_RECIPIENTS,
-      subject: 'Avengers is here.',
+      subject: "Avengers is here.",
       html:
         '<p>Hey! This is a reminder that new theatre has started screening <a href="https://in.bookmyshow.com/buytickets/pvr-forum-sujana-mall-kukatpally-hyderabad/cinema-hyd-PVSF-MT/20190426">Avengers: End Game</a></p>'
     };
@@ -102,19 +112,40 @@ function sendHyderabadMail() {
   }
 }
 
+// Send a telegram message
+function sendMumbaiTelegramMessage() {
+  var api = new telegram({
+    token: process.env.TOKEN
+  });
+
+  api
+    .sendMessage({
+      chat_id: process.env.CHATID,
+      text: "Avengers Theatres list updated !"
+    })
+    .then(function(data) {
+      console.log(util.inspect(data, false, null));
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
 async function doHyderabadStuff() {
-  console.log('Doing Hyderabad...');
+  console.log("Doing Hyderabad...");
   clearInterval(timer);
   setInterval(async () => {
-    console.log('Making the request');
+    console.log("Making the request");
     await request(
-      'https://in.bookmyshow.com/buytickets/pvr-forum-sujana-mall-kukatpally-hyderabad/cinema-hyd-PVSF-MT/20190426',
+      "https://in.bookmyshow.com/buytickets/pvr-forum-sujana-mall-kukatpally-hyderabad/cinema-hyd-PVSF-MT/20190426",
       (error, response, html) => {
-        if (!error & (response.statusCode == 200)) {
-          var $ = cheerio.load(html);
-          if ($('#venuelist').children('.list').length > hyderabad) {
-            hyderabad++;
-            return sendHyderabadMail();
+        if (response) {
+          if (!error & (response.statusCode == 200)) {
+            var $ = cheerio.load(html);
+            if ($("#venuelist").children(".list").length > hyderabad) {
+              hyderabad++;
+              return sendHyderabadMail();
+            }
           }
         }
       }
@@ -124,8 +155,37 @@ async function doHyderabadStuff() {
   return true;
 }
 
+async function doMumbaiStuff() {
+  console.log("Doing Mumbai...");
+  clearInterval(timer);
+  setInterval(async () => {
+    console.log("Making the request Mumbai " + count++);
+    try {
+      await request(
+        "https://in.bookmyshow.com/buytickets/avengers-endgame-mumbai/movie-mumbai-ET00100668-MT/20190502",
+        (error, response, html) => {
+          if (response) {
+            if (!error & (response.statusCode == 200)) {
+              var $ = cheerio.load(html);
+              if ($("#venuelist").children(".list").length > mumbai) {
+                mumbai++;
+                return sendMumbaiTelegramMessage();
+              }
+            }
+          }
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }, timer);
+
+  return true;
+}
+
 http.createServer().listen(3000, () => {
-  console.log('Server at PORT 3000 started.');
+  console.log("Server at PORT 3000 started.");
   doBangaloreStuff();
   doHyderabadStuff();
+  doMumbaiStuff();
 });
